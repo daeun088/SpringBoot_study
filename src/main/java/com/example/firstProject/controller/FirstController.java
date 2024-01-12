027d59ba54c66a112894caadca06218e86424e8b
daeun088 <kde2329@naver.com>
2024-01-12 16:28:13 +0900

    템플릿화 하기 전

--- src/main/java/com/example/firstProject/controller/FirstController.java
+++ src/main/java/com/example/firstProject/controller/FirstController.java
@@ -1,12 +1,23 @@
 package com.example.firstProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 public class FirstController {
 
-    public String niceToMeetYou(){
-        return "";
+    @GetMapping("/hi")//hi는 URL 주소
+
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "홍팍");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("username", "홍팍");
+        return "goodbye";
     }
 }
