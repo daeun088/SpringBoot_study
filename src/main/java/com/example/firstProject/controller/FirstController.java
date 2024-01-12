c045949be06c64fe1b694a2ba927fbd77bcf24bb
daeun088 <kde2329@naver.com>
2024-01-12 15:35:33 +0900

    Initial commit

---
+++ src/main/java/com/example/firstProject/controller/FirstController.java
@@ -0,0 +1,12 @@
+package com.example.firstProject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+public class FirstController {
+
+    public String niceToMeetYou(){
+        return "";
+    }
+}
